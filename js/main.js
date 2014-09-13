d5a0392bce0838491b3cd79e80dc97ce43c53c1f
“Steven <Dunn.Steven1@gmail.com>
2014-09-12 20:54:23 -0700

    Added Header Logos, continued to re-style the header and links. Remove mult-color underline for now.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 $(document).ready(function() 
 {
+    //If we're using the dark theme...
+    //change the logo
+    $(".dark").find(".sdunnLogo").attr("src", "images/sdunnLogo-teal.png");
 
+    $(".dark").find(".sdunnLogo").hover(function() {
+        $(this).attr("src", "images/smileLogo-white.png");
+    }, function()
+    {
+        $(this).attr("src", "images/sdunnLogo-teal.png");
+    });
+
+    //If we're using the light theme...
+    $(".sdunnLogo").hover(function() {
+        $(this).attr("src", "images/smileLogo-orange.png");
+    }, function()
+    {
+        $(this).attr("src", "images/sdunnLogo-black.png");
+    });
 });
