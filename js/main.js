df6ef5b9b43cbea943c983e94c6101e248d82624
“Steven <Dunn.Steven1@gmail.com>
2014-09-12 21:37:49 -0700

    Added .less mixins for default colors. Changed colors further.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,47 @@
 $(document).ready(function() 
 {
-    //If we're using the dark theme...
-    //change the logo
-    $(".dark").find(".sdunnLogo").attr("src", "images/sdunnLogo-teal.png");
-
-    $(".dark").find(".sdunnLogo").hover(function() {
-        $(this).attr("src", "images/smileLogo-white.png");
-    }, function()
+    var mainLogo = $(".sdunnLogo");
+    var body = $("body");
+    $(".toggleTheme").click(function()
     {
-        $(this).attr("src", "images/sdunnLogo-teal.png");
+        body.toggleClass("dark");
+
+        //Change logo to black or teal based on light/dark theme
+        if(body.hasClass("dark"))
+        {
+            $(this).text("Light Theme");
+            mainLogo.attr("src", "images/sdunnLogo-teal.png");
+        }
+        else
+        {
+            $(this).text("Dark Theme");
+            mainLogo.attr("src", "images/sdunnLogo-black.png");
+        }
     });
 
-    //If we're using the light theme...
-    $(".sdunnLogo").hover(function() {
-        $(this).attr("src", "images/smileLogo-orange.png");
+    mainLogo.hover(function()
+    {
+        //On hover change logo to white/orange based on light/dark theme
+        if(body.hasClass("dark"))
+        {
+            mainLogo.attr("src", "images/smileLogo-white.png");
+        }
+        else
+        {
+            mainLogo.attr("src", "images/smileLogo-orange.png");
+        }
+
     }, function()
     {
-        $(this).attr("src", "images/sdunnLogo-black.png");
+        //Revert back to original logo colors when mouse leaves
+        if(body.hasClass("dark"))
+        {
+            mainLogo.attr("src", "images/sdunnLogo-teal.png");
+        }
+        else
+        {
+            mainLogo.attr("src", "images/sdunnLogo-teal.png");
+        }
+
     });
 });
