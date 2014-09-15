003f1e876908d780078566681956405fa6d57bbf
“Steven <Dunn.Steven1@gmail.com>
2014-09-14 21:26:21 -0700

    Mobile nav now drops down and links are centered. Links now change background color when clicked (i.e. actually on mouse down).

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() 
 {
     var body = $("body");
+    var navItems = $("nav ul li");
     $(".toggleTheme").click(function()
     {
         body.toggleClass("dark");
@@ -15,4 +16,29 @@ $(document).ready(function()
             $(this).text("Dark Theme");
         }
     });
+
+    //This code only affects the mobile version of the site.
+    $("header").click(function()
+    {
+        $(".mobile-logo").toggleClass("selected");
+        $(".mobile-button").toggleClass("selected");
+        if($(".mobile-logo").hasClass("selected"))
+        {
+            $(this).find("nav ul").show();
+        }
+        else
+        {
+            $(this).find("nav ul").hide();
+        }
+    });
+    navItems.mousedown(function()
+    {
+        $(this).css("background", "#fff");
+        $(this).find("a").css("color", "#40919b");
+    });
+    navItems.mouseout(function()
+    {
+        $(this).css("background", "#000");
+    }
+    );
 });
