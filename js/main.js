7e153f73b07400a33912bcbb6d5f955018e9d7c1
“Steven <Dunn.Steven1@gmail.com>
2014-09-14 17:19:15 -0700

    Logo hover is now done with CSS instead of JS, since using JS for this was pointless and caused problems coding the mobile behavior for the nav.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() 
 {
-    var mainLogo = $(".sdunnLogo");
     var body = $("body");
     $(".toggleTheme").click(function()
     {
@@ -10,38 +9,10 @@ $(document).ready(function()
         if(body.hasClass("dark"))
         {
             $(this).text("Light Theme");
-            mainLogo.attr("src", "images/sdunnLogo-teal.png");
         }
         else
         {
             $(this).text("Dark Theme");
-            mainLogo.attr("src", "images/sdunnLogo-black.png");
         }
-    });
-
-    mainLogo.hover(function()
-    {
-        //On hover change logo to white/orange based on light/dark theme
-        if(body.hasClass("dark"))
-        {
-            mainLogo.attr("src", "images/smileLogo-white.png");
-        }
-        else
-        {
-            mainLogo.attr("src", "images/smileLogo-orange.png");
-        }
-
-    }, function()
-    {
-        //Revert back to original logo colors when mouse leaves
-        if(body.hasClass("dark"))
-        {
-            mainLogo.attr("src", "images/sdunnLogo-teal.png");
-        }
-        else
-        {
-            mainLogo.attr("src", "images/sdunnLogo-teal.png");
-        }
-
     });
 });
