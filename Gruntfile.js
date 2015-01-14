32d89e009e44b64471abb62cba7cb0240cd594fa
“Steven <Dunn.Steven1@gmail.com>
2015-01-13 16:32:44 -0800

    Javascript files are now combined and compressed into a single "index.min.js" file. jQuery, jQueryUI, and jQuery Mobile are still being loaded seperately as needed.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,22 +4,36 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        //Minify JS Configuration
+        //Concat multiple files into a single index file
+        concat: {
+            dist: {
+                src: [
+                    //'js/*.js' // All JS in the js folder. Not used
+                    //jQuery/ui/mobile is currently loaded as needed, so it doesn't appear here
+                    'js/jquery.stellar.js',
+                    'js/jquery.inview.min.js',
+                    'js/main.js'
+                ],
+                dest: 'js/build/index.js'
+            }
+        },
+
+        //Minify the JS
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
+                src: 'js/build/index.js',
+                dest: 'js/build/index.min.js'
             }
         }
     });
 
     //Load plugins
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    //Tasks to execute when using the "grunt" command
-    grunt.registerTask('default', ['uglify']);
-
+    //Tasks to execute when using the "grunt" command with no arguments
+    grunt.registerTask('default', ['concat','uglify']);
 };
